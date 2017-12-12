6336e618583b0f7f8664cae7d2490ef6fe927e55
ThinMintz <rachel.rodriguez05@gmail.com>
2017-12-11 21:54:41 -0600

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,3 +4,7 @@ A POST routes /api/friends. This will be used to handle incoming survey results.
 This route will also be used to handle the compatibility logic.*/
 
 var path = require('path');
+
+	app.get('/api/friends', function (req, res) {
+		res.json(friends);
+	});
