92a6b49e00f86856a6ff31d74c0394ada4e8e6c5
ThinMintz <rachel.rodriguez05@gmail.com>
2017-12-11 20:15:00 -0600

    updating

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var friends = [
+{
+  "name":"Fry",
+  "photo":"https://pbs.twimg.com/profile_images/618633920960016384/1xNc_2-Y.jpg",
+  "scores":[
+      1,
+      1,
+      1,
+      1,
+      1,
+    ]
+},
+
+{
+  "name":"Leela",
+  "photo":"http://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/turanga-leela-futurama-92.5.jpg",
+  "scores":[
+      2,
+      2,
+      2,
+      2,
+      2
+    ]
+},
+
+{
+  "name":"Bender",
+  "photo":"https://s-media-cache-ak0.pinimg.com/originals/fd/7b/ed/fd7bedc096282be66275f442506eac36.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+
+
+]
