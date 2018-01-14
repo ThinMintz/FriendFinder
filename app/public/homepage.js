963351ae76e19236693b2ea1b8a414abffbdd74b
ThinMintz <rachel.rodriguez05@gmail.com>
2018-01-14 14:17:58 -0600

    trying to get app to work

--- app/public/homepage.js
+++ app/public/homepage.js
@@ -1,10 +1,5 @@
 // Add active if input element has been pre-populated on document ready
-    $(document).ready(function() {
-      Materialize.updateTextFields();
-    });
 
 $(document).ready(function() {
     $('select').material_select();
   });
-
-$('select').material_select('destroy');
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,17 @@ A POST routes /api/friends. This will be used to handle incoming survey results.
 This route will also be used to handle the compatibility logic.*/
 
 var path = require('path');
+var friends = require("../data/friends.js");
 
+module.exports = function(app) {
 	app.get('/api/friends', function (req, res) {
 		res.json(friends);
 	});
+
+	app.post('/api/friends', function (req, res) {
+		var userName = req.body;
+		console.log(userName);
+		
+		};
+	});
+};
